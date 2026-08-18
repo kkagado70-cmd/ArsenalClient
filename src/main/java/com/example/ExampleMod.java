76ba199b6942d0a951be3c0c94f212a66c13c657
kkagado70-cmd <kkagado70@gmail.com>
2026-08-18 00:29:02 -0300

    Atualizar o ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,30 +1,16 @@
-package com.example;
+package com.example.automace;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import net.minecraft.resources.Identifier;
+public class AutoMaceMod implements ModInitializer {
+    public static final String MOD_ID = "automace";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("AutoMace standalone Fabric initialized for Kovak.");
+        AutoMaceLogic.init();
+    }
 }
