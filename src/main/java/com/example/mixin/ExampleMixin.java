d0ec6aca34269dcad0cda6fae61d5932e64e7bac
kkagado70-cmd <kkagado70@gmail.com>
2026-08-18 00:31:18 -0300

    Atualizar o ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,16 @@
-package com.example.mixin;
+package com.example.automace.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.example.automace.AutoMaceLogic;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        AutoMaceLogic.onTick();
+    }
 }
